f0713e2c7571ac3188deee52f46900ea34c26864
mlopez <ml.3012@gmail.com>
2023-08-30 16:17:21 -0300

    input-mp3

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,30 @@
+"use client"
+
+import { useState } from "react";
+import axios from "axios";
+
 export default function Home() {
+  const [ youtubeUrl, setYoutubeUrl ] = useState("");
+
+  const handleClickMp3 = async () => {
+    try { 
+      const data = {video_url: youtubeUrl}
+      
+      await axios.post("http://127.0.0.1:8000/converter/", data, {withCredentials: true});
+      setYoutubeUrl("");
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+      <input type="text" 
+      placeholder="URL"
+      value={youtubeUrl}
+      onChange={(e) => setYoutubeUrl(e.target.value)}/>
+      <button onClick={handleClickMp3}>MP3</button>
     </main>
   )
 }
