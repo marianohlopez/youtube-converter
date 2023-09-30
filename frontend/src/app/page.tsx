37c8ec27d5a10e0638004ff3b485a73a6254fa3d
mlopez <ml.3012@gmail.com>
2023-09-30 20:03:24 -0300

    video-input

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,16 +4,31 @@ import { useState } from "react";
 import axios from "axios";
 
 export default function Home() {
-  const [ youtubeUrl, setYoutubeUrl ] = useState("");
+  const [ youtubeUrlMp3, setYoutubeUrlMp3] = useState("");
+  const [ youtubeUrlVideo, setYoutubeUrlVideo] = useState("");
 
   const handleClickMp3 = async () => {
     try { 
       const data = {
-        video_url: youtubeUrl,
+        video_url: youtubeUrlMp3,
       };
       
-      await axios.post("http://127.0.0.1:8000/converter/", data);
-      setYoutubeUrl("");
+      await axios.post("http://127.0.0.1:8000/converter/audio", data);
+      setYoutubeUrlMp3("");
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
+  const handleClickVideo = async () => {
+    try { 
+      const data = {
+        video_url: youtubeUrlVideo,
+      };
+      
+      await axios.post("http://127.0.0.1:8000/converter/video", data);
+      setYoutubeUrlVideo("");
     }
     catch (err) {
       console.log(err);
@@ -23,10 +38,15 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <input type="text" 
-      placeholder="URL"
-      value={youtubeUrl}
-      onChange={(e) => setYoutubeUrl(e.target.value)}/>
+      placeholder="MP3 URL"
+      value={youtubeUrlMp3}
+      onChange={(e) => setYoutubeUrlMp3(e.target.value)}/>
       <button onClick={handleClickMp3}>MP3</button>
+      <input type="text" 
+      placeholder="VIDEO URL"
+      value={youtubeUrlVideo}
+      onChange={(e) => setYoutubeUrlVideo(e.target.value)}/>
+      <button onClick={handleClickVideo}>VIDEO</button>
     </main>
   )
 }
